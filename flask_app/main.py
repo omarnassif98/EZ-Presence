f0da1c644730577644939aa756d31040e7f60a48
omarnassif98 <omar.nassif98@gmail.com>
2021-05-29 14:26:40 -0400

    Updated Main.py
    Never thought I'd have to simplify a Hello World program.
    No CLI, no python env, just pip Flask and run this file

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    socketApp.run(app, port=8080)
