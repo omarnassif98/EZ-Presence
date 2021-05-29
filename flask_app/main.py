1b5a9fda9b9a418d66e36d61952fd10cc6e335e2
Dillon Morse <dillon@inoffensiveandwarm.com>
2021-05-29 14:43:45 -0400

    Serve dummy landing page for route "/"

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def landing_page():
+    return render_template("landing_page.html")
 
 
 if __name__ == '__main__':
